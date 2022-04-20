a81a11ceb554ec4a4f047669b5cc60613a0314de
mrcaseb <roemerle@gmail.com>
2022-04-20 17:04:35 +0200

    export pbp parser

--- R/usfl_game_data.R
+++ R/usfl_game_data.R
@@ -15,5 +15,24 @@ load_usfl_game_data <- function(usfl_game_id){
   game_data
 }
 
-
+#' Extract and Parse PBP Date from Raw USFL Game Data
+#'
+#' @param raw_game_data Raw data loaded with [load_usfl_game_data()]
+#'
+#' @return A tibble with pbp
+#' @export
+usfl_parse_pbp <- function(raw_game_data){
+  tidyr::unnest_longer(raw_game_data$pbp$sections, groups, values_to = "drive") |>
+    dplyr::rename(qtr = title) |>
+    tidyr::unnest_wider(drive, names_sep = "_") |>
+    tidyr::unnest_longer(drive_plays) |>
+    tidyr::unnest_longer(drive_plays) |>
+    tidyr::unnest(drive_plays) |>
+    dplyr::rename_with(
+      .fn = function(x) paste0("play_", x),
+      .cols = !tidyselect::starts_with("drive_")
+    ) |>
+    dplyr::mutate(game_id = raw_game_data$header$id) |>
+    dplyr::select(game_id, dplyr::everything())
+}
 
